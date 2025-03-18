d55b2510ca16f1995cb239fd438c085b364c25a3
kb <kb0968237@gmail.com>
2025-03-18 10:27:35 +0530

    created class for products and clothing

--- data/products.js
+++ data/products.js
@@ -1,3 +1,5 @@
+import { formatCurrency } from '../scripts/utils/price.js';
+
 export function getProduct(productId){
   let matchingProduct;
 
@@ -8,6 +10,50 @@ export function getProduct(productId){
   });
 
   return matchingProduct;
+}
+
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+  getStarsUrl() {
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  getPrice() {
+    return `$${formatCurrency(this.priceCents)}`;
+  }
+
+  extraInfoHTML() {
+    return '';
+  }
+}
+class Clothing extends Product{
+  sizeChartLink;
+
+  constructor(productDetails) {
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML() {
+    return `
+      <a href="${this.sizeChartLink}" target="_blank">
+        Size chart
+      </a>
+    `;
+  }
 }
 
 export const products = [
@@ -669,4 +715,6 @@ export const products = [
       "mens"
     ]
   }
-];
+].map(productDetails => {
+  return productDetails.type === 'clothing' ? new Clothing(productDetails) : new Product(productDetails);
+});
