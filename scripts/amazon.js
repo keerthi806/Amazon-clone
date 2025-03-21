9206eb774f4e752e53e9ec7a4e03633780ac8e47
kb <kb0968237@gmail.com>
2025-03-21 19:44:50 +0530

    made search interative

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -9,9 +9,21 @@ loadProducts().then(products => {
 
 function renderProductsPage(){
   updateCartQuantity();
+
+  const url = new URL(window.location.href);
+  const search = url.searchParams.get('search');
+
+  let filteredProducts = products;
+
+  if(search) {
+    filteredProducts = products.filter((product) => {
+      return product.name.includes(search);
+    });
+  }
+
   let productsHTML = '';
 
-  products.forEach((product) => {
+  filteredProducts.forEach((product) => {
     productsHTML += 
       `<div class="product-container">
         <div class="product-image-container">
@@ -88,6 +100,11 @@ function renderProductsPage(){
       }, 1000);
 
     });
+  });
+
+  document.querySelector('.js-search-button').addEventListener('click', () => {
+    const search = document.querySelector('.js-search-input').value;
+    window.location.href = `amazon.html?search=${search}`;
   });
 
   function updateCartQuantity() {
