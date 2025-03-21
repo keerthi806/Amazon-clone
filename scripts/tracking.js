06b058d68686259282007c452a6066410def6667
kb <kb0968237@gmail.com>
2025-03-21 13:33:14 +0530

    made tracking dynamic

--- scripts/tracking.js
+++ scripts/tracking.js
@@ -1,6 +1,7 @@
 import {products, loadProducts} from '../data/products.js';
 import { orders } from '../data/orders.js';
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
+import { getCartQuantity } from '../data/cart.js'
 
 loadProducts().then(() => {
   renderTracking();
@@ -57,23 +58,85 @@ function renderTracking() {
       <img class="product-image" src="${matchingProduct.image}">
 
       <div class="progress-labels-container">
-        <div class="progress-label">
+      </div>
+
+      <div class="progress-bar-container">
+        <div class="progress-bar js-progress-bar"></div>
+      </div>
+    </div>
+  `;
+
+  document.querySelector('.js-cart-quantity').textContent = `${getCartQuantity()}`
+
+  //console.log(trackingHTML);
+  document.querySelector('.js-main').innerHTML = trackingHTML;
+
+  const progress = calculateProgress(matchingOrder, trackingProduct);
+  console.log(progress);
+
+  document.querySelector('.js-progress-bar').style.width = `${progress}%`;
+  updateProgressStatus(progress);
+
+}
+
+function calculateProgress(order, trackingProduct) {
+  let currentTime = dayjs().add(24, 'hours');
+  const orderTime = dayjs(order.orderTime);
+  const deliveryTime = dayjs(trackingProduct.estimatedDeliveryTime);
+
+  const elapsedTime = currentTime.diff(orderTime, 'minute');
+  const totalTime = deliveryTime.diff(orderTime, 'minute');
+
+  // console.log(currentTime);
+  // console.log(orderTime);
+  // console.log(deliveryTime);
+  // console.log(elapsedTime);
+  // console.log(totalTime);
+
+  const progress = Math.round((elapsedTime / totalTime) * 100);
+  
+  return progress >= 100 ? 100 : progress; 
+}
+
+function updateProgressStatus(progress){
+  if(progress >= 0 && progress <= 49){
+    document.querySelector('.progress-labels-container').innerHTML = 
+      `
+        <div class="progress-label current-status">
           Preparing
         </div>
-        <div class="progress-label current-status">
+        <div class="progress-label">
           Shipped
         </div>
         <div class="progress-label">
           Delivered
         </div>
+      `;
+  } else if(progress >=50 && progress <= 99){
+    document.querySelector('.progress-labels-container').innerHTML = 
+    `
+      <div class="progress-label">
+        Preparing
       </div>
-
-      <div class="progress-bar-container">
-        <div class="progress-bar"></div>
+      <div class="progress-label current-status">
+        Shipped
       </div>
-    </div>
-  `;
-
-  console.log(trackingHTML);
-  document.querySelector('.js-main').innerHTML = trackingHTML;
+      <div class="progress-label">
+        Delivered
+      </div>
+    `;
+  } else {
+    document.querySelector('.progress-labels-container').innerHTML = 
+    `
+      <div class="progress-label">
+        Preparing
+      </div>
+      <div class="progress-label">
+        Shipped
+      </div>
+      <div class="progress-label current-status">
+        Delivered
+      </div>
+    `;
+  }
 }
